10ab36f199235351aafaffd3504c221861507a9f
Sanjay Yadav <sanjay.yadav1@rsystems.com>
2019-11-15 18:11:00 +0530

    Form and index updated.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,20 +1,20 @@
 class AccountsController < ApplicationController
   def index
-    @logs = Account.all
+    @accounts = Account.all
   end
 
   def new
-    @log = Account.new
+    @account = Account.new
   end
 
   def show
   end
 
   def create
-    @log = Account.new(account_params)
+    @account = Account.new(account_params)
 
     respond_to do |format|
-      if @log.save
+      if @account.save
         format.html  { redirect_to(accounts_index_url, :notice => 'Account was successfully created.') }
         format.json  { render :json => accounts_index_url, :status => :created, :location => @account }
       else
